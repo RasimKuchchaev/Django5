c8a36344449446bf9dedbfcb3561597414dfb2b8
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-21 16:36:42 +0300

    2.5 Переадресация и отправка статусных кодов

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,9 @@ from blog import views
 
 
 
-urlpatterns = [path('', views.index),
-               path("user/", views.user),
-               
+urlpatterns = [path("about/", views.about),
+               path("contact/", views.contact),
+               path("details/", views.details),
+               path("index/<int:id>/", views.index),
+               path("access/<int:age>/", views.access),
              ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
 
+def index(request, id):
+    people = [None, "Bob", "Sam", "Tom"]
+    # если пользователь найден, возвращает его
+    if id in range(1, len(people)):
+        return HttpResponse(people[id])
+    # если нет, возвращает ошибку 404
+    return HttpResponseNotFound("Not Found")
 
-def index(request):
-    return HttpResponse("<h2>Главная</h2>")
+def access(request, age):
+    # если возвраст не входит в диапозон 1-110, посылаем ошибку 400
+    if age not in range(1, 111):
+        return HttpResponseBadRequest("Некорректные данные")
+    # если возраст больше 17, то доступ разрешен
+    if (age > 17):
+        return HttpResponse("Доступ разрешен")
+    # если нет, то возвращаем ошибку 403
+    else:
+        return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")
+     
 
-# def user(request):
-#     age = request.GET.get("age")
-#     name = request.GET.get("name")
-#     return HttpResponse(F"<h2>Name: {name} Age: {age}</h2>")
+def about(request):
+    return HttpResponse("About")
 
-def user(request):
-    age = request.GET.get("age", 0)
-    name = request.GET.get("name", "Undefined")
-    return HttpResponse(F"<h2>Name: {name} Age: {age}</h2>")
+def contact(request):
+    return HttpResponseRedirect("/about")
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
