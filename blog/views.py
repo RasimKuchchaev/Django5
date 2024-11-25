3ce409531f55a5dc2e70afac2dad68c26723070a
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-25 11:00:39 +0300

    2.6 Шаблоны, TemplateResponse

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.http import HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    # return render(request, 'blog/index.html')
+    return TemplateResponse(request, 'blog/index.html')
