4d11f728361fecd8df3b1375e1ad423c5ee16586
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-25 15:17:31 +0300

    2.6 Шаблоны, Передача в шаблон сложных данных

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    data = {"header": "Hello Django", "message": "Welcome to Python"}
-    return render(request, 'blog/index.html', context=data)
+    header = "Данные пользователя"  # обычная переменная
+    langs = ["Python", "Java", "C#"]  # список
+    user = {"name": "Tom", "age": 23}  # словарь
+    address = ("Абрикосовая", 23, 45)  # кортеж
+
+    data = {"header": header, "langs": langs, "user": user, "address": address}
+    return render(request, "blog/index.html", context=data)
