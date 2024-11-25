34411f814172889a6610cd56398bd958786b4a48
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-25 15:08:09 +0300

    2.6 Шаблоны, Передача данных в шаблоны

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.http import HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
 from django.shortcuts import render
-from django.template.response import TemplateResponse
 
 
 def index(request):
-    # return render(request, 'blog/index.html')
-    return TemplateResponse(request, 'blog/index.html')
+    data = {"header": "Hello Django", "message": "Welcome to Python"}
+    return render(request, 'blog/index.html', context=data)
