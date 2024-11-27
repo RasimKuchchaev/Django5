aa92f86a421bea20d9969de6acb5fc68b82a9d3a
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-27 10:57:32 +0300

    2.6 Шаблоны

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    header = "Данные пользователя"  # обычная переменная
-    langs = ["Python", "Java", "C#"]  # список
-    user = {"name": "Tom", "age": 23}  # словарь
-    address = ("Абрикосовая", 23, 45)  # кортеж
+    # if
+    data = {"age": 40}
+    # return render(request, "blog/index.html", context=data)
 
-    data = {"header": header, "langs": langs, "user": user, "address": address}
-    return render(request, "blog/index.html", context=data)
+    # for
+    cat = ["Python", "Java", "C++", "C#", "Kotlin", "Go", "Rust", "JS", "PHP"]
+    return render(request, "blog/index.html", context={"cat": cat, "age": data})
