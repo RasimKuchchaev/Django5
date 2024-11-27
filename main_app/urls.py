a8ae513b014e376c1d74f18cd290d885e432952e
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-27 17:43:10 +0300

    DZ1

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from main_app import views
 
 urlpatterns = [ path("", views.index),
-               path("about/", views.about),
-               path("contact/", views.contact),
-               path("details/", views.details),
-               path("support/", views.support),
+                path("contact/", views.contact),
+                path("profile/", views.profile),
                ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,20 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.template.response import  TemplateResponse
+from django.shortcuts import  render
 
 
 def index(request):
-    return HttpResponse("Index")
-
-
-def about(request):
-    return HttpResponsePermanentRedirect("/")
-
+    return TemplateResponse(request,"main_app/index.html")
 
 def contact(request):
-    return HttpResponseRedirect("/about/")
+    data= 'Телефон: +79123456789, E-Mail: admin@email.com'
+    return render(request,"main_app/contact.html", context={"data":data})
 
+def profile(request):
+    name=request.GET.get('name', 'Дмитрий')
+    age=request.GET.get('age', '39')
+    phone=request.GET.get('phone', '+79123456789')
+    email=request.GET.get('email', 'dmitry@email.com11')    
+    return render(request,"main_app/profile.html", 
+                  context={'name': name, 'age': age, 
+                           'phone': phone, 'email': email})
 
-def details(request):
-    return HttpResponsePermanentRedirect("/")
-
-def support(request):
-    return HttpResponseRedirect("/contact/")
