ba18f0cfa82e895bc00f7d9a4cba70bb71cbd424
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-27 16:11:03 +0300

    2.6 Теги Django

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    # if
-    data = {"age": 40}
-    # return render(request, "blog/index.html", context=data)
-
-    # for
-    cat = ["Python", "Java", "C++", "C#", "Kotlin", "Go", "Rust", "JS", "PHP"]
-    return render(request, "blog/index.html", context={"cat": cat, "age": data})
+    return render(request, "blog/index.html", context={"body": "<h1>Hello World!</h1>"})
