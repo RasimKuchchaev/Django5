1485c2b9b240ccde65127a2573ebe3904b2da8e3
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-11-27 23:03:50 +0300

    2.7 Шаблоны, Статичные файлы

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from blog import views
+from django.views.generic import TemplateView
 
 
 
-urlpatterns = [path("", views.index),               
+urlpatterns = [path("", views.index),  
+               path("about/", TemplateView.as_view(template_name="blog/about.html", extra_context={'header':"О сайте"})),  
+               path("contact/", TemplateView.as_view(template_name="blog/contact.html"))         
              ]
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "blog/index.html", context={"body": "<h1>Hello World!</h1>"})
+    return render(request, "blog/index.html")
