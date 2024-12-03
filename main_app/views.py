f3f00500ab5c5a0acbcc83d1a9a0cb401ce717f8
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-12-03 16:15:32 +0300

    DZ if for

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import  render
 
 
 def index(request):
-    profile_data=['Павел', 45, '+79234567891', 'pavel@pavel.com']
-    return render(request,"main_app/index.html", context={'profile_data':profile_data})
+    user_list = [{'name': 'Дмитрий', 'experience': 9},
+                {'name': 'Павел',   'experience': 5},
+                {'name': 'Алексей', 'experience': 3},
+                {'name': 'Иван',    'experience': 0},
+                {'name': 'Денис',   'experience': 2},
+                {'name': 'Игорь',   'experience': 7},
+                {'name': 'Руслан',  'experience': 1},
+                {'name': 'Евгений', 'experience': 4},
+                {'name': 'Андрей',  'experience': 2},
+                {'name': 'Николай', 'experience': 8}]
+    return render(request,"main_app/index.html", context={'user_list':user_list})
 
 def contact(request):
     data= 'Телефон: +79123456789, E-Mail: admin@email.com'
